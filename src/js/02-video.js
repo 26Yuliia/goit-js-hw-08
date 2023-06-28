bb058a9a397497b890f645a38822c28c7777712a
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-06-28 21:46:36 +0300

    add 3 t

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,60 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
 });
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
 };
 
-player.on('play', onPlay);
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
+console.log(time);
+
+//player.on('play', function () {
+//  console.log('played the video!');
+//});
+
+//player.getVideoTitle().then(function (title) {
+//// console.log('title:', title);
+//});
+
+//const onPlay = function (data) {
+// /data is an object containing properties specific to that event
+//};
+
+////player.on('play', onPlay);
+
+//player.on('eventName', function (data) {
+// duration: 61.857;
+// percent: 0;
+// seconds: 0;
+//});
+//player
+// .setCurrentTime(30.456)
+// .then(function (seconds) {
+//   // seconds = the actual time that the player seeked to
+// })
+//.catch(function (error) {
+// switch (error.name) {
+// case 'RangeError':
+//  duration: 1000;
+// percent: 0;
+// seconds: 0;
+// break;
+
+// default:
+// some other error occurred
+//  break;
+// }
+// });
+//
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const LOCAL_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input [name="email"]');
+const textareaEl = document.querySelector('textarea [name="message"]');
 
+let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+
+formEl, addEventListener('input', throttle(storageFormData, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+reloadPage();
+
+function storageFormData(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (
+    e.target.elements.email.value === '' ||
+    e.target.elements.message.value === ''
+  ) {
+    return alert(`Заповніть всі поля!`);
+  }
+  console.log(formData);
+
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+  formData = {};
+}
+
+function reloadPage() {
+  if (formData) {
+    let { email, message } = formEl.elements;
+    email.value = formData.email || '';
+    message.value = formData.message || '';
+  }
+}
